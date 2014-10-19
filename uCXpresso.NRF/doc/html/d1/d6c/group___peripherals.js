9f689b47b361469ced18fdd9bb36d6497049aa8b
廖晨凱 <jason@embeda.com.tw>
2014-10-19 16:41:47 +0800

    Beta 1, document updated

--- uCXpresso.NRF/doc/html/d1/d6c/group___peripherals.js
+++ uCXpresso.NRF/doc/html/d1/d6c/group___peripherals.js
@@ -159,6 +159,18 @@ var group___peripherals =
       [ "transfer", "da/dc3/classspi_master.html#a794db5c033bededec6e93fc5a1a29c9b", null ],
       [ "transfer", "da/dc3/classspi_master.html#a14ed1edc76b7169efc3650d867ab2a4e", null ]
     ] ],
+    [ "spiSlave", "da/d31/classspi_slave.html", [
+      [ "spiSlave", "da/d31/classspi_slave.html#a07708aa22816dff0bad30452b1fd09f8", null ],
+      [ "bitOrder", "da/d31/classspi_slave.html#ad730e3de1fa6ca429ced93d75c800519", null ],
+      [ "def_tx_character", "da/d31/classspi_slave.html#a4b0c751d5d66351672211ca6321d9e64", null ],
+      [ "isThread", "da/d31/classspi_slave.html#a26a76c241a4d62d2efdac57d2cfe4c0f", null ],
+      [ "isValid", "da/d31/classspi_slave.html#a18cf254bf2030ba75735238fc1a31f4d", null ],
+      [ "mode", "da/d31/classspi_slave.html#a601df989a52b39c6c39aa48f09423688", null ],
+      [ "onTransactionCompleted", "da/d31/classspi_slave.html#a6268b685c20ad68d55e427940e129382", null ],
+      [ "orc_tx_character", "da/d31/classspi_slave.html#aa6fd95af7bbe2cc9acf91e0cc99f86d2", null ],
+      [ "transact", "da/d31/classspi_slave.html#a7e9c6ee1aaf4084ccb7badcfa22ed3f0", null ],
+      [ "wait", "da/d31/classspi_slave.html#ad205b5fc70a705c7dd1ff093ff87a587", null ]
+    ] ],
     [ "CStream", "d4/d16/class_c_stream.html", [
       [ "CStream", "d4/d16/class_c_stream.html#a147900f253c57d3223ae1220e0336808", null ],
       [ "available", "d4/d16/class_c_stream.html#afdbf8d786e4aac9b06afe3447427a2ae", null ],
