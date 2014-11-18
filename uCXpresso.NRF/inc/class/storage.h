22de0c087ddef7037b6ce686c0054c4eac42acc7
廖晨凱 <jason@embeda.com.tw>
2014-11-18 15:25:35 +0800

    v1.0.1 rc3 updated.

--- uCXpresso.NRF/inc/class/storage.h
+++ uCXpresso.NRF/inc/class/storage.h
@@ -1,8 +1,18 @@
 /*
- * flash.h
- *
- *  Created on: Aug 7, 2014
- *      Author: Jason
+ ===============================================================================
+ Name        : Storage data to flash memory
+ Author      : uCXpresso
+ Version     : v1.0.0
+ Date		 : 2014/10/18
+ Copyright   : Copyright (C) www.embeda.com.tw
+ Description :
+ ===============================================================================
+ 	 	 	 	 	 	 	 	 History
+ ---------+---------+--------------------------------------------+-------------
+ DATE     |	VERSION |	DESCRIPTIONS							 |	By
+ ---------+---------+--------------------------------------------+-------------
+ 2014/10/18	v1.0.0	First Edition for nano51822						Jason
+ ===============================================================================
  */
 
 #ifndef FLASH_H_
@@ -29,7 +39,7 @@ public:
 	/// @cond PRIVATE
 	//
 	virtual ~CStorage();
-	virtual void onEvent(uint8_t op_code, uint32_t result, uint8_t * p_data, uint32_t data_len);
+	virtual void onEvent(uint8_t op_code, uint32_t result, uint8_t * p_data, uint32_t data_len) { NOTHING }
 protected:
 	uint16_t   	m_size;
 	xHandle 	m_handle;
--- uCXpresso.NRF/inc/uCXpresso.h
+++ uCXpresso.NRF/inc/uCXpresso.h
@@ -20,7 +20,7 @@ extern "C" {
 #define uCXpresso_VER_MAJOR		1
 #define uCXpresso_VER_MINOR		0
 #define uCXpresso_VER_REV		1
-#define uCXpresso_VER_RC		2
+#define uCXpresso_VER_RC		3
 #define uCXpresso_VER_BUILD		"2014/11/18"
 #define uCXpresso_VER_STR		"V1.0.1 rc3"
 
