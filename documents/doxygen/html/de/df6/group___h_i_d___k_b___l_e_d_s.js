633555d7dc44abbf80b668c9ca2aafb2b678f5e8
廖晨凱 <jason@embeda.com.tw>
2014-11-27 10:46:12 +0800

    document updated.

--- documents/doxygen/html/de/df6/group___h_i_d___k_b___l_e_d_s.js
+++ documents/doxygen/html/de/df6/group___h_i_d___k_b___l_e_d_s.js
@@ -1,6 +1,6 @@
 var group___h_i_d___k_b___l_e_d_s =
 [
-    [ "KB_LED_CAPS", "de/df6/group___h_i_d___k_b___l_e_d_s.html#ga9504bf71e0ea83cf2613039402cfafc9", null ],
+    [ "KB_LED_CAPS_LOCK", "de/df6/group___h_i_d___k_b___l_e_d_s.html#ga18a03981eeb2049a25017ff984667b1a", null ],
     [ "KB_LED_COMPOSE", "de/df6/group___h_i_d___k_b___l_e_d_s.html#gaf72b5e2f5f4edf002e413af87e41f912", null ],
     [ "KB_LED_KANA", "de/df6/group___h_i_d___k_b___l_e_d_s.html#ga2a54479b60c6980afd892d137a1ca291", null ],
     [ "KB_LED_NUM_LOCK", "de/df6/group___h_i_d___k_b___l_e_d_s.html#gac4e3c27d1b99e14f82464db666eb9996", null ],
