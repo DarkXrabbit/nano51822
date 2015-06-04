ec671d8d02080e133843a90eaea3f60fd5303f2e
廖晨凱 <jason@embeda.com.tw>
2015-06-04 22:35:22 +0800

    Add the nano51822-udk board's text number table.

--- uCXpresso.NRF/inc/config/board.h
+++ uCXpresso.NRF/inc/config/board.h
@@ -1,5 +1,5 @@
 /*
- * board pin define
+ * board pin assignment
  *
  */
 
@@ -7,6 +7,43 @@
 #define BOARD_H_
 
 #include <class/ble/ble_device.h>
+
+#ifdef BOARD_NANO51822_UDK
+#define BUTTON_PIN_0	0
+#define BUTTON_PIN_1	30
+#define LED_PIN_0		18
+#define LED_PIN_1		19
+#define LED_PIN_2		20
+#define LED_PIN_3		21
+
+// Board's Text No. to GPIO Pin No.
+#define LED1			LED_PIN_0
+#define LED2			LED_PIN_1
+#define LED3			LED_PIN_2
+#define LED4			LED_PIN_3
+#define BTN1			BUTTON_PIN_0
+#define	BTN2			BUTTON_PIN_1
+#define P12				7
+#define P13				12
+#define P14				13
+#define P15				14
+#define P16				15
+#define P17				16
+#define P18				17
+#define P19				22
+#define P20				23
+#define P24				24
+#define P25				25
+#define AD0				1
+#define AD1				2
+#define AD2				3
+#define AD3				4
+#define AD4				5
+#define AD5				6
+#define P30				30
+#define SCL				28
+#define SDA				29
+#endif
 
 #ifdef BOARD_LILYPAD
 #define BUTTON_PIN_0	0		// DFU Button
@@ -22,22 +59,6 @@
 #define LED_PIN_2		20
 #define LED_PIN_3		21
 #define LED_PIN_4		22
-#endif
-
-#ifdef BOARD_NANO51822_UDK
-#define BUTTON_PIN_0	0
-#define BUTTON_PIN_1	30
-#define LED_PIN_0		18
-#define LED_PIN_1		19
-#define LED_PIN_2		20
-#define LED_PIN_3		21
-
-#define LED1			LED_PIN_0
-#define LED2			LED_PIN_1
-#define LED3			LED_PIN_2
-#define LED4			LED_PIN_3
-#define BTN1			BUTTON_PIN_0
-#define	BTN2			BUTTON_PIN_1
 #endif
 
 #endif /* BOARD_H_ */
