d87ec8c8f3860525876c3db58f629ee0aaa47814
廖晨凱 <jason@embeda.com.tw>
2015-06-16 17:36:14 +0800

    Add write_wo_resp for the RX char. of bleServiceUART class.

--- uCXpresso.NRF/inc/class/ble/ble_service_uart.h
+++ uCXpresso.NRF/inc/class/ble/ble_service_uart.h
@@ -3,7 +3,7 @@
  Name        : ble_service_uart.h
  Author      : uCXpresso
  Version     : v1.0.3
- Date		 : 2015/03/31
+ Date		 : 2015/06/16
  Copyright   : Copyright (C) www.embeda.com.tw
  Description : BLE UART Service class
  ===============================================================================
@@ -14,6 +14,7 @@
  2014/10/12	v1.0.0	First Edition for nano51822						Jason
  2014/10/27 v1.0.1	Update characteristic uuid.						Jason
  2015/03/31	v1.0.2	More safe transmit for large data.				Jason
+ 2015/06/16 v1.0.3	Add write_wo_resp for the prperty of RX Char.	Jason
  ===============================================================================
  */
 
@@ -39,6 +40,7 @@ public:
 	/**@brief bleServiceUART constructor
 	 *
 	 * @param[in] ble is a bleDevice object.
+	 * @param[in] write_wo_resp Set the write (rx char.) property without response to speed up the received speed.
 	 * @param[in] base_uuid Point to a 16 bytes array that contents are the @ref ble_uuid128_t UUID.
 	 * @param[in] tx_char_uuid The uuid of the tx characteristic. (device to App)
 	 * @param[in] rx_char_uuid The uuid of the rx characteristic. (app to device)
@@ -47,12 +49,14 @@ public:
 	 *
 	 */
 	bleServiceUART(bleDevice &ble,
+				   bool write_wo_resp = false,
 				   ble_uuid128_t const *base_uuid = &nus_base_uuid,
 				   uint16_t service_uuid = BLE_UUID_UART_SERVICE,
 				   uint16_t tx_char_uuid = BLE_UUID_UART_TX_CHARACTERISTIC,
 				   uint16_t rx_char_uuid = BLE_UUID_UART_RX_CHARACTERISTIC,
 				   size_t tx_fifo_size = 64,
 				   size_t rx_fifo_size = 64);
+
 	/**
 	 * @brief Check the TX char. be listened.
 	 * @return true if TX char. be listened by app.
--- uCXpresso.NRF/inc/uCXpresso.h
+++ uCXpresso.NRF/inc/uCXpresso.h
@@ -21,7 +21,7 @@ extern "C" {
 #define uCXpresso_VER_MINOR		0
 #define uCXpresso_VER_REV		7
 #define uCXpresso_VER_RC		3
-#define uCXpresso_VER_BUILD		"2014/6/11"
+#define uCXpresso_VER_BUILD		"2014/6/16"
 #define uCXpresso_VER_STR		"V1.0.7"
 
 //
