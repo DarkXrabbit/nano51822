ec6f6aaa568059981449f4c8a7648f802fc1440a
廖晨凱 <jason@embeda.com.tw>
2015-06-22 11:26:01 +0800

    Add example "ble_app_adc".

---
+++ examples/ble/ble_app_adc/src/main.cpp
@@ -0,0 +1,139 @@
+/*
+===============================================================================
+ Name        : BLE Battery Service
+ Author      : uCXpresso
+ Version     : v1.0.0
+ Copyright   : www.ucxpresso.net
+ Description : ADC via BLE
+===============================================================================
+ 	 	 	 	 	 	 	 	 History
+ ---------+---------+--------------------------------------------+-------------
+ DATE     |	VERSION |	DESCRIPTIONS							 |	By
+ ---------+---------+--------------------------------------------+-------------
+ 2015/6/22 v1.0.0	First Edition.									LEO
+ ===============================================================================
+ */
+
+#include <uCXpresso.h>
+
+#ifdef DEBUG
+#include <debug.h>
+#include <class/serial.h>
+#define DBG dbg_printf
+#else
+#define DBG(...)
+#endif
+
+// TODO: insert other include files here
+#include <class/ble/ble_device.h>
+#include <class/ble/ble_service.h>
+#include <class/pin.h>
+
+#include <class/adc.h>
+#include <class/ble/ble_service_uart.h>
+#include <class/ble/ble_conn_params.h>
+#include <class/timeout.h>
+#include <class/console.h>
+
+// TODO: insert other definitions and declarations here
+#define DEVICE_NAME                         "nano51822"            	/**< Name of device. Will be included in the advertising data. */
+#define MANUFACTURER_NAME                   "uCXpresso.NRF"        	/**< Manufacturer. Will be passed to Device Information Service. */
+#define APP_ADV_INTERVAL                    500                		/**< The advertising interval (in ms). */
+#define APP_COMPANY_IDENTIFIER				0x0059          		/**< Company identifier for Nordic Semi. as per www.bluetooth.org. */
+
+//
+// Board LED define
+//
+//#define BOARD_PCA10001
+//#define BOARD_LILYPAD
+#define BOARD_NANO51822_UDK
+#include <config/board.h>
+
+//
+// Main Routine
+//
+int main(void) {
+#ifdef DEBUG
+	CSerial ser;		// declare a UART object
+	ser.enable();
+	CDebug dbg(ser);	// Debug stream use the UART object
+	dbg.start();
+#endif
+	//
+	// SoftDevice
+	//
+	bleDevice ble;	// use internal 32.768KHz for SoftDevice
+	ble.enable();	// enable BLE SoftDevice stack
+
+	// GAP
+	ble.m_gap.settings(DEVICE_NAME);	// set Device Name on GAP
+	ble.m_gap.tx_power(BLE_TX_0dBm);
+
+	//
+	// Add UART Service
+	//
+	bleServiceUART nus(ble);
+
+	//
+	// Connection Parameters Update negotiation
+	//
+	bleConnParams conn(ble);
+
+	//
+	// BLE Advertising
+	//
+	ble.m_advertising.interval(APP_ADV_INTERVAL);			// set advertising interval
+//	ble.m_advertising.add_uuid_to_complete_list(nus);		// add bat object to the uuid list of advertising
+	ble.m_advertising.update();								// update advertisement data
+
+	// Start advertising
+	ble.m_advertising.start();
+
+	//
+	// Enable the Analog
+	//
+	CAdc::init();
+	CAdc::enable();
+
+	//
+	// LED
+	//
+	CPin led0(LED_PIN_0);
+	CPin led1(LED_PIN_1);
+	led0.output();
+	led1.output();
+
+	CTimeout tmLED, tmADC;
+
+	uint16_t value;
+	Console	 console(nus);	// console use the nus stream.
+
+	//
+    // Enter main loop.
+	//
+    while(1) {
+    	//
+    	// BLE Battery Service
+    	//
+    	if ( nus.isAvailable() ) {
+    		if ( tmADC.isExpired(1000) ) {
+    			tmADC.reset();
+    			if ( CAdc::read(AD0, value) ) {
+    				console.printf("AD0=%d\n", value);
+    				led1.toggle();
+    			}
+    		}
+    	} else led1 = LED_OFF;
+
+    	// Negotiate the "connection parameters update"
+    	conn.negotiate();
+
+    	//
+    	// blink LED
+    	//
+    	if ( tmLED.isExpired(500) ) {
+    		tmLED.reset();
+    		led0.toggle();
+    	}
+    }
+}
